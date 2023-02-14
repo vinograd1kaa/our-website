17b6a685232d790740b9b6f73aa716352686e4fa
examplyz <slubsku2.0@gmail.com>
2023-02-14 20:13:56 +0100

    Fixed app running errors.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {FC} from "react";
+
+interface HeaderProps {
+
+}
+
+
+const Header:FC<HeaderProps> = (props) =>{
+    return (<div></div>)
+}
+
+
+export default Header
