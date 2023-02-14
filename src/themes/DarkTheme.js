5c3e8c2e212346ad9f117f6917924d590c3f4891
examplyz <slubsku2.0@gmail.com>
2023-02-14 19:59:05 +0100

    Add dark and light themes.

---
+++ src/themes/DarkTheme.js
@@ -0,0 +1,16 @@
+export const darkTheme =  {
+    name: "dark",
+    colors: {
+        outline          : "#959EAE",
+        textColor        : "#D3D5D8",
+        background       : "#191C21",
+        grayBG           : "#212833",
+        menuButton       : "#4E5D78",
+        textHeader       : "#4E5D78",
+        textTitle        : "#BDC2CC",
+        button           : "#377DFF",
+        sendButton       : "#EBF2FF",
+        notifications    : "#FF5630",
+        menuButtonEnabledText : "#D3D5D8"
+    }
+}
---
+++ src/themes/LightTheme.js
@@ -0,0 +1,16 @@
+export const lightTheme =  {
+    name: "light",
+    colors: {
+        outline          : "#B8BEC9",
+        background       : "#E5E5E5",
+        grayBG           : "#F6F7F8",
+        menuButton       : "#4E5D78",
+        textHeader       : "#4E5D78",
+        textTitle        : "#BDC2CC",
+        textColor        : "#6B778E",
+        button           : "#377DFF",
+        sendButton       : "#EBF2FF",
+        notifications    : "#FF5630",
+        menuButtonEnabledText: "#D3D5D8"
+    }
+}
